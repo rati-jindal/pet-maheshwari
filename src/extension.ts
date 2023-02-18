c97be3a2bb6811865b9deab7a05e2a42b9c3f163
rati-jindal <iamratijindal@gmail.com>
2023-02-18 23:22:21 +0530

    Added Notifications

--- src/extension.ts
+++ src/extension.ts
@@ -1,11 +1,24 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import * as axios from 'axios';
 const animals = ['🐶', '🐱', '🐰', '🐻', '🐨', '🦊', '🐼', '🐯', '🦁', '🐮'];
 
 const getRandomAnimal = () => {
     const randomIndex = Math.floor(Math.random() * animals.length);
     return animals[randomIndex];
+};
+const getMotivationalQuote = async () => {
+    const response = await axios.default.get('https://api.quotable.io/random');
+    return response.data.content;
+};
+
+const showNotification = async () => {
+    const animal = getRandomAnimal();
+    const quote = await getMotivationalQuote();
+    const message = `${animal} ${quote}`;
+	// console.log(message);
+	vscode.window.showInformationMessage(message);
 };
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
@@ -20,8 +33,8 @@ export function activate(context: vscode.ExtensionContext) {
 	// The commandId parameter must match the command field in package.json
 	let disposable = vscode.commands.registerCommand('pet-maheshwari.helloWorld', () => {
 		// The code you place here will be executed every time your command is executed
+		setInterval(showNotification, 0.1 * 60 * 1000);
 		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from Pet Maheshwari!');
 	});
 
 	context.subscriptions.push(disposable);
