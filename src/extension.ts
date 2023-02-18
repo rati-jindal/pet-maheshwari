26d24adcf668f2c64adb56b0a1543355a086c872
rati-jindal <iamratijindal@gmail.com>
2023-02-18 23:05:41 +0530

    Added Random Pets Generator

--- src/extension.ts
+++ src/extension.ts
@@ -1,7 +1,12 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+const animals = ['🐶', '🐱', '🐰', '🐻', '🐨', '🦊', '🐼', '🐯', '🦁', '🐮'];
 
+const getRandomAnimal = () => {
+    const randomIndex = Math.floor(Math.random() * animals.length);
+    return animals[randomIndex];
+};
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
